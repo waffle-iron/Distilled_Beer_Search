30f590ff72e58c958b032052845715a268dd89c6
Gary Byrne <gary.byrne39@mail.dcu.ie>
2017-05-16 16:38:54 +0100

    Return data to frontend

--- routes.js
+++ routes.js
@@ -4,16 +4,17 @@
 // add api key to .env
 require('dotenv').config();
 
-const express = require('express');
+import express from 'express';
 const router = express.Router();
-const request = require('request');
+import request from 'request';
 
 router.get('/', function(req, res, next) {
     res.render('index.html');
 });
 
 router.get('/randombeer', function(req, res, next) {
-    request.get("http://api.brewerydb.com/v2/beer/random/?key=" + process.env.API_KEY, function(err, request, response) {
+    const url = "http://api.brewerydb.com/v2/beer/random/?key=" + process.env.API_KEY;
+    request.get(url, function(err, request, response) {
         res.send(response);
     });
 });
@@ -21,7 +22,7 @@ router.get('/randombeer', function(req, res, next) {
 router.get('/search/:type/:query', function(req, res, next) {
     let url = "http://api.brewerydb.com/v2/search?q=" + req.params.query+ "&type=" + req.params.type + "&key=" + process.env.API_KEY;
     request.get(url, function(err, request, response) {
-        res.json(response);
+        res.send(response);
     });
 });
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+"use strict";
+
+import { Component} from '@angular/core';
+import { BeerService } from './beer.service';
 
 @Component({
     selector: 'app-root',
-    template: '<h2>Hello World</h2>'
+    templateUrl: 'app.component.html',
+    providers: [BeerService]
 })
+
 export class AppComponent {
 
+    message = 'This is the sample message.';
+    title: string = "test";
 
+    obj: any = {};
+
+    constructor(private _beerService: BeerService) {}
+
+    getRandom() {
+        this._beerService.getRandomBeer()
+            .then((result) => {
+                    this.obj = <JSON>result.data;
+                    console.log(this.obj);
+            })
+            .catch((error) => console.error(error));
+    }
+
+    ngOnInit() {
+        this.getRandom();
+        while(this.obj != {}){
+            this.getRandom();
+        }
+    }
 }
